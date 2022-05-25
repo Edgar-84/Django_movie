0bc285152a785a486f9aab51a8ed4c1c58f2aadd
edgar84 <edgar.tomko.97@mail.ru>
2022-05-25 07:37:25 +0300

    create flatpages for simple pages

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "django.contrib.flatpages",
     "ckeditor",
     "ckeditor_uploader",
     "movies",
@@ -53,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.flatpages.middleware.FlatpageFallbackMiddleware"
 ]
 
 ROOT_URLCONF = "django_movie.urls"
@@ -199,6 +202,8 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+SITE_ID = 1
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- django_movie/urls.py
+++ django_movie/urls.py
@@ -22,6 +22,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path("pages/", include('django.contrib.flatpages.urls')),
     path("", include("movies.urls")),
 ]
 
